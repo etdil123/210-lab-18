19bf8d4fe1454e27dcd6e70c4bd4baa2a001320a
etdil123 <ethandilk@MacBook-Air.attlocal.net>
2024-10-06 11:33:13 -0700

    10 min commit

--- main.cpp
+++ main.cpp
@@ -3,3 +3,29 @@
 using namespace std;
 
 
+struct Node { 
+    float movieRating;
+    string reviewComments;
+    Node * next;
+};
+
+int main() {
+    
+    char repeat;
+    
+    while (char != 'n' || char != 'N'){
+        int userDecision;
+
+        cout << "Select linked list method to be used: " << endl;
+        cout << "   [1] New nodes are added at the head of the linked list" << endl;
+        cout << "   [2] New nodes are added to the tail of the linked list" << endl;
+        cout << "   Your Choice: ";
+        cin >> userDecision;
+
+
+        cout << "Enter another review? Y/N: "
+        
+    }
+
+    return 0;
+}
